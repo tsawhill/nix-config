b14a9b418960ac5dc68bf06831813b9a311dce3e
Taylor Sawhill <git@tsawhill.org>
2025-03-30 15:55:10 -0700

    taylor-nix - Generation 41 Successful Build

--- modules/shared/user_definition/taylor/home-manager/default.nix
+++ modules/shared/user_definition/taylor/home-manager/default.nix
@@ -47,6 +47,7 @@
         vim = "nvim";
         g = "git";
         update-system = "cd ~/.config/nixos/ && sudo nix flake update && sudo nixos-rebuild switch && git add -A && git commit -m \"$(hostname) - Generation $(nix-env --list-generations | grep current | awk '{print $1}') Successful Build\" && git push ; cd -";
+        rebuild-system = "cd ~/.config/nixos/ && sudo nixos-rebuild switch && git add -A && git commit -m \"$(hostname) - Generation $(nix-env --list-generations | grep current | awk '{print $1}') Successful Build (No flake update)\" && git push ; cd -";
       };
     };
 
