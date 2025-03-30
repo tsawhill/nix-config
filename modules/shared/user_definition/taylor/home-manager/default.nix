f63c8d09990b147acc74ecdcb711d599a7d69e77
Taylor Sawhill <git@tsawhill.org>
2025-03-30 16:03:51 -0700

    taylor-nix - Generation 243 Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/default.nix
+++ modules/shared/user_definition/taylor/home-manager/default.nix
@@ -46,8 +46,8 @@
       shellAliases = {
         vim = "nvim";
         g = "git";
-        update-system = "cd ~/.config/nixos/ && sudo nix flake update && sudo nixos-rebuild switch && git add -A && git commit -m \"$(hostname) - Generation $(nix-env --list-generations | grep current | awk '{print $1}') Successful Build\" && git push ; cd -";
-        rebuild-system = "cd ~/.config/nixos/ && sudo nixos-rebuild switch && git add -A && git commit -m \"$(hostname) - Generation $(nix-env --list-generations | grep current | awk '{print $1}') Successful Build (No flake update)\" && git push ; cd -";
+        update-system = "cd ~/.config/nixos/ && sudo nix flake update && sudo nixos-rebuild switch && git add -A && git commit -m \"$(hostname) - Generation $(nixos-rebuild list-generations | grep current | awk '{print $1}') Successful Build\" && git push ; cd -";
+        rebuild-system = "cd ~/.config/nixos/ && sudo nixos-rebuild switch && git add -A && git commit -m \"$(hostname) - Generation $(nixos-rebuild list-generations | grep current | awk '{print $1}') Successful Build (No flake update)\" && git push ; cd -";
       };
     };
 
