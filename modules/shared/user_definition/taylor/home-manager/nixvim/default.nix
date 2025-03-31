9e4a58601396f8289ce5ee1210f296b059e0dfbe
Taylor Sawhill <git@tsawhill.org>
2025-03-31 15:30:20 -0700

    taylor-nix - Generation 257 Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/nixvim/default.nix
+++ modules/shared/user_definition/taylor/home-manager/nixvim/default.nix
@@ -3,6 +3,28 @@
     enable = true;
 
     colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
+    plugins = {
+      telescope = {
+        enable = true;
+      };
+      treesitter = {
+        enable = true;
+      };
+      lualine = {
+        enable = true;
+      };
+      cmp = {
+        enable = true;
+      };
+      cmp-nvim-lsp = {
+        enable = true;
+      };
+      lsp = {
+        enable = true;
+      };
+      commentary = {
+        enable = true;
+      };
+    };
   };
 }
