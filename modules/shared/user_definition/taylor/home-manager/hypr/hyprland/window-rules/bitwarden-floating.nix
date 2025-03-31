48f868df1b140fa7627f10694332a6d821377fc4
Taylor Sawhill <git@tsawhill.org>
2025-03-31 12:54:34 -0700

    taylor-nixlaptop - Generation 73 Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/bitwarden-floating.nix
+++ modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/bitwarden-floating.nix
@@ -1,6 +1,6 @@
 {
   # Window rules for floating slideout bluetooth control from taskbar
   wayland.windowManager.hyprland.settings.windowrulev2 = [
-    "float,title:^(Ext.*Bit.*)$"
+    "float,title:^(.*Bitwarden.*)$"
   ];
 }
