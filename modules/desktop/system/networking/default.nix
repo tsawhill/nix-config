109435d37174828c48ac311bcf7bcfe8a41f93b8
Taylor Sawhill <git@tsawhill.org>
2025-04-03 08:31:09 -0700

    taylor-nix - Generation 278 Successful Build (No flake update)

--- modules/desktop/system/networking/default.nix
+++ modules/desktop/system/networking/default.nix
@@ -11,17 +11,17 @@
           LOG_PREFIX="WiFi Auto-Toggle"
           ETHERNET_INTERFACE="eno2"
 
-          if [ "$1" = "$ETHERNET_INTERFACE" ]; then
-              case "$2" in
-                  up)
-                      echo "$LOG_PREFIX ethernet up"
-                      # nmcli radio wifi off
-                      ;;
-                  down)
-                      echo "$LOG_PREFIX ethernet down"
-                      # nmcli radio wifi on
-                      ;;
-              esac
+          # if [ "$1" = "$ETHERNET_INTERFACE" ]; then
+          #     case "$2" in
+          #         up)
+          #             echo "$LOG_PREFIX ethernet up"
+          #             # nmcli radio wifi off
+          #             ;;
+          #         down)
+          #             echo "$LOG_PREFIX ethernet down"
+          #             # nmcli radio wifi on
+          #             ;;
+          #     esac
           # elif [ "$(nmcli -g GENERAL.STATE device show $ETHERNET_INTERFACE)" = "20 (unavailable)" ]; then
           #     echo "$LOG_PREFIX failsafe"
           #     nmcli radio wifi on
