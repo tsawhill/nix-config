31258affb26327b4beffe6aeaa2df9abe450de67
Taylor Sawhill <git@tsawhill.org>
2025-04-02 22:08:29 -0700

    taylor-nix - Generation 269 Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/nixvim/plugins/cmp.nix
+++ modules/shared/user_definition/taylor/home-manager/nixvim/plugins/cmp.nix
@@ -10,6 +10,15 @@
         { name = "buffer"; }
         { name = "cmdline"; }
       ];
+      settings.mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-e>" = "cmp.mapping.close()";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      };
     };
     cmp-nvim-lsp = {
       enable = true;
