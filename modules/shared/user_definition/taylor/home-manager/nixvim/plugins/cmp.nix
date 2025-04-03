ef06c7c97cf72de500641cc7eb4ba9db46fa4004
Taylor Sawhill <git@tsawhill.org>
2025-04-02 21:47:23 -0700

    taylor-nix - Generation 268 Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/nixvim/plugins/cmp.nix
+++ modules/shared/user_definition/taylor/home-manager/nixvim/plugins/cmp.nix
@@ -2,8 +2,22 @@
   programs.nixvim.plugins = {
     cmp = {
       enable = true;
+      autoEnableSources = true;
+      settings.sources = [
+        { name = "nvim-lsp"; }
+        { name = "vim-lsp"; }
+        { name = "git"; }
+        { name = "buffer"; }
+        { name = "cmdline"; }
+      ];
     };
     cmp-nvim-lsp = {
+      enable = true;
+    };
+    cmp-vim-lsp = {
+      enable = true;
+    };
+    cmp-git = {
       enable = true;
     };
   };
