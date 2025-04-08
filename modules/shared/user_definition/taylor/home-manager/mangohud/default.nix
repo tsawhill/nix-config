7b1c21b787a0cac263bfe8696922f771e41b876a
Taylor Sawhill <git@tsawhill.org>
2025-04-08 12:52:26 -0700

    taylor-nix - Generation 347 Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/mangohud/default.nix
+++ modules/shared/user_definition/taylor/home-manager/mangohud/default.nix
@@ -4,9 +4,12 @@
     enableSessionWide = true;
     settings = {
       font_file =
-        "${pkgs.nerd-fonts.daddy-time-mono}/share/fonts/truetype/NerdFonts/DaddyTimeMono/DaddyTimeMonoNerdFont-Regular.ttf";
+        "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/DaddyTimeMono/DaddyTimeMonoNerdFont-Regular.ttf";
       position = "top-right";
-      font_scale = 1.5;
+      font_size = 28;
+      frame_timing = 0;
+      height = 200;
+      width = 200;
       gpu_stats = true;
       cpu_stats = true;
       output_folder = /tmp;
