023989baf16a1295a9f921273cdffe6ceb287d33
Taylor Sawhill <git@tsawhill.org>
2025-04-09 09:44:31 -0700

    taylor-nix - Generation 357 Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/mangohud/default.nix
+++ modules/shared/user_definition/taylor/home-manager/mangohud/default.nix
@@ -9,7 +9,7 @@
       frame_timing = 0;
       height = 200;
       width = 200;
-      blacklist = "walker";
+      blacklist = "Walker";
       gpu_stats = true;
       gpu_temp = true;
       cpu_stats = true;
