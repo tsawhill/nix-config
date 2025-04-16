d4245468f0348fa6aed16f44ea87d66dae79e640
Taylor Sawhill <git@tsawhill.org>
2025-04-16 09:01:15 -0700

    taylor-nix - Generation 370 Successful Build (No flake update)

--- modules/desktop/user_definition/taylor/home-manager/hyprland/monitors.nix
+++ modules/desktop/user_definition/taylor/home-manager/hyprland/monitors.nix
@@ -1,6 +1,6 @@
 {
   wayland.windowManager.hyprland.settings.monitor = [
-    "DP-1,3440x1440@165,0x0,1,vrr,2,bitdepth,10"
+    "DP-1,3440x1440@165,0x0,1,vrr,2"
     "DP-2,2560x1440@165,3440x0,1"
     "HDMI-A-1,3840x2160@60,0x0,1"
   ];
