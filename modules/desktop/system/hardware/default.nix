3bf9a02f8509c50399b113b7ca1789a2c67639f7
Taylor Sawhill <git@tsawhill.org>
2025-05-15 21:30:05 -0700

    taylor-nix - Generation 390 Successful Build (No flake update)

--- modules/desktop/system/hardware/default.nix
+++ modules/desktop/system/hardware/default.nix
@@ -4,4 +4,16 @@
 {
   # For Logitech G923 Racing Wheel
   hardware.new-lg4ff.enable = true;
+
+  # Enable ntfs for windows disk
+  boot.supportedFilesystems = [ "ntfs" ];
+  fileSystems."/mnt/windows" = {
+    device = "/dev/nvme1n1p3";
+    fsType = "ntfs-3g";
+    options = [
+      "rw"
+      "uid=1000"
+    ];
+  };
+
 }
