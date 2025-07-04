6e1161c6f513dfe828f936c8fe6e792d6c6209f9
Taylor Sawhill <git@tsawhill.org>
2025-07-04 15:04:03 -0700

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/desktop/user_definition/taylor/home-manager/hyprland/monitors.nix
+++ modules/desktop/user_definition/taylor/home-manager/hyprland/monitors.nix
@@ -1,11 +1,38 @@
 { lib, ... }:
 {
-  wayland.windowManager.hyprland.settings.monitor = [
-    "DP-1,3440x1440@165,0x0,1,vrr,2"
-    "DP-2,2560x1440@165,3440x0,1"
-    "HDMI-A-1,3840x2160@60,0x0,1"
-    "HEADLESS-1, 3840x2160@60, -3840x0, 1"
-  ];
+  # wayland.windowManager.hyprland.settings.monitor = [
+  #   "DP-1,3440x1440@165,0x0,1,vrr,2"
+  #   "DP-2,2560x1440@165,3440x0,1"
+  #   "HDMI-A-1,3840x2160@60,0x0,1"
+  #   "HEADLESS-1, 3840x2160@60, -3840x0, 1"
+  # ];
+  wayland.windowManager.hyprland.extraConfig = ''
+    monitorv2 {
+      output=DP-1
+      mode=3440x1440@165
+      position=0x0
+      scale=1
+      vrr=2
+    }
+    monitorv2 {
+      output=DP-2
+      mode=2560x1440@165
+      position=3440x0
+      scale=1
+    }
+    monitorv2 {
+      output=HDMI-A-1
+      mode=3840x2160@60
+      position=0x0
+      scale=1
+    }
+    monitorv2 {
+      output=HEADLESS-1
+      mode=3840x2160@60
+      position=-3840x0
+      scale=1
+    }
+  '';
   # wayland.windowManager.hyprland.settings.monitorv2 = [
   #   {
   #     output = "DP-1";
@@ -33,5 +60,5 @@
   #     scale = 1;
   #   }
 
-#   ];
+  #   ];
 }
