e1805065a3cbdfe1b1b5b883e02712ba44781aeb
Taylor Sawhill <git@tsawhill.org>
2025-08-06 22:25:24 -0700

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/desktop/system/software/default.nix
+++ modules/desktop/system/software/default.nix
@@ -1,3 +1,9 @@
+{ pkgs, ... }:
 {
   imports = [ ./sunshine.nix ];
+  environment.systemPackages = with pkgs; [ eddie ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "dotnet-sdk-6.0.428"
+    "dotnet-runtime-6.0.36"
+  ];
 }
