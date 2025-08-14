a1e9afb54137351da26ec51236a76524e6c63a70
Taylor Sawhill <git@tsawhill.org>
2025-08-14 13:42:17 -0700

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/system/software/gamescope.nix
+++ modules/shared/system/software/gamescope.nix
@@ -1,7 +1,13 @@
 { pkgs-gamescope, pkgs, ... }:
 {
-  environment.systemPackages = [
-    # pkgs-gamescope.gamescope
-    pkgs.gamescope_git
-  ];
+  # environment.systemPackages = [
+  #   # pkgs-gamescope.gamescope
+  #   pkgs.gamescope_git
+  # ];
+  programs.gamescope = {
+    enable = true;
+    package = pkgs.gamescope.overrideAttrs (_: {
+      NIX_CFLAGS_COMPILE = [ "-fno-fast-math" ];
+    });
+  };
 }
