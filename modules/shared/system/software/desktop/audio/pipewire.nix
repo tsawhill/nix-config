fc2ec1a1185ea1e136eed2f18c556b769220ff8a
Taylor Sawhill <git@tsawhill.org>
2025-08-13 21:04:38 -0700

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/system/software/desktop/audio/pipewire.nix
+++ modules/shared/system/software/desktop/audio/pipewire.nix
@@ -14,10 +14,10 @@
       "92-low-latency" = {
         "context.properties" = {
           "default.clock.rate" = 48000;
-          "default.clock.quantum" = 64;
-          "default.clock.quantum-limit" = 64;
-          "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 64;
+          "default.clock.quantum" = 128;
+          "default.clock.quantum-limit" = 128;
+          "default.clock.min-quantum" = 64;
+          "default.clock.max-quantum" = 128;
         };
       };
     };
@@ -31,14 +31,14 @@
           }
         ];
         "pulse.properties" = {
-          "pulse.min.req" = "32/48000";
-          "pulse.default.req" = "64/48000";
-          "pulse.max.req" = "64/48000";
-          "pulse.min.quantum" = "32/48000";
-          "pulse.max.quantum" = "64/48000";
+          "pulse.min.req" = "64/48000";
+          "pulse.default.req" = "128/48000";
+          "pulse.max.req" = "128/48000";
+          "pulse.min.quantum" = "64/48000";
+          "pulse.max.quantum" = "128/48000";
         };
         "stream.properties" = {
-          "node.latency" = "64/48000";
+          "node.latency" = "128/48000";
           "resample.quality" = 1;
         };
       };
