d6f25b42409327468bbf6c9f20e3313e17f38f3b
Taylor Sawhill <git@tsawhill.org>
2025-11-24 06:54:47 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/hypr/hyprland/autostart.nix
+++ modules/shared/user_definition/taylor/home-manager/hypr/hyprland/autostart.nix
@@ -13,6 +13,9 @@
     "sleep 10; vesktop"
     "feishin"
 
+    # Wallpapers
+    ''mpvpaper -o "no-audio --loop-playlist shuffle --framedrop=vo=5" DP-1 /home/taylor/.config/wallpapers/2560x1440''
+
     # Bluetooth does not consistently start on boot because the device is not detected until after networking starts.
     "rfkill unblock 0;sleep 15; rfkill unblock 0"
   ];
