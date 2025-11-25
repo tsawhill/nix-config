4dc83366dbec659f723e51b0a6075747c977133e
Taylor Sawhill <git@tsawhill.org>
2025-11-25 08:24:24 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/transparent-inactive.nix
+++ modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/transparent-inactive.nix
@@ -3,5 +3,6 @@
   wayland.windowManager.hyprland.settings.windowrulev2 = [
     "opacity 1.0 0.95, class:.+"
     "opacity 1.0 1.0, title:^(.*- YouTube — Ablaze Floorp)$"
+    "opacity 1.0 1.0, class:^(foot)$"
   ];
 }
