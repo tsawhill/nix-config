47675babcefed07cd70b1c47f1bfc9e362904d6a
Taylor Sawhill <git@tsawhill.org>
2025-11-25 08:05:52 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/transparent-inactive.nix
+++ modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/transparent-inactive.nix
@@ -1,6 +1,6 @@
 {
   # window rules for floating inactive window
   wayland.windowManager.hyprland.settings.windowrulev2 = [
-    "opacity 1.0 0.92, class:.+"
+    "opacity 1.0 0.95, class:.+"
   ];
 }
