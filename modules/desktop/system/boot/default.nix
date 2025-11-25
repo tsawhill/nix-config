27369913c2172baa6cc47fae9d466b4909bfb487
Taylor Sawhill <git@tsawhill.org>
2025-11-25 10:09:17 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/desktop/system/boot/default.nix
+++ modules/desktop/system/boot/default.nix
@@ -4,4 +4,8 @@
   ];
   boot.loader.efi.canTouchEfiVariables = true;
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
+  boot.kernelParams = [
+    "video=DP-1:2560x1440@359.99899"
+    "video=DP-2:3440x1440@120.00000"
+  ];
 }
