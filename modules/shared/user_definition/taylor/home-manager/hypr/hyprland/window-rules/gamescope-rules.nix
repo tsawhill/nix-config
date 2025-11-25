29972df887f908f3fd167bf947cefcb2c0d32f29
Taylor Sawhill <git@tsawhill.org>
2025-11-25 09:59:22 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/gamescope-rules.nix
+++ modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/gamescope-rules.nix
@@ -3,6 +3,7 @@
   wayland.windowManager.hyprland.settings.windowrulev2 = [
     "idleinhibit focus,class:^(gamescope)$"
     "immediate, class:^(gamescope)$"
+    "immediate, class:^(cs2)$"
     # "immediate, focus:1"
   ];
 }
