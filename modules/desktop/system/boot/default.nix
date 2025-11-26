cc89e56e9e549310ccdcaae491b6a00bb0bd05b0
Taylor Sawhill <git@tsawhill.org>
2025-11-26 14:56:22 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/desktop/system/boot/default.nix
+++ modules/desktop/system/boot/default.nix
@@ -5,8 +5,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
   boot.kernelParams = [
-    "video=DP-1:2560x1440@359.99899"
-    "video=DP-2:3440x1440@120.00000"
+    "video=DP-1:2560x1440@360"
+    "video=DP-2:3440x1440@165"
     "amdgpu.ppfeaturemask=0xfffd7fff"
   ];
 }
--- modules/desktop/user_definition/taylor/home-manager/hyprland/monitors.nix
+++ modules/desktop/user_definition/taylor/home-manager/hyprland/monitors.nix
@@ -26,7 +26,7 @@
     monitorv2 {
       output=desc:Dell Inc. AW3423DWF 3D442S3
       # mode=modeline 531.41 3440 3448 3480 3520 1440 1496 1504 1510 +hsync -vsync
-      mode=3440x1440@144Hz
+      mode=3440x1440@165Hz
       position=auto-right
       scale=1
       # vrr=1
