a54a0b2dc13760207d4a8de3b1f642f4c26770c4
Taylor Sawhill <git@tsawhill.org>
2025-12-03 09:36:44 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/transparent-inactive.nix
+++ modules/shared/user_definition/taylor/home-manager/hypr/hyprland/window-rules/transparent-inactive.nix
@@ -4,6 +4,7 @@
     "opacity 1.0 0.90, class:.+"
     "opacity 1.0 1.0, title:^(.*- YouTube — Ablaze Floorp)$"
     "opacity 1.0 1.0, title:^(.*tv — Ablaze Floorp)$"
+    "opacity 1.0 1.0, title:^(.*Kick — Ablaze Floorp)$"
     "opacity 1.0 0.8, class:^(foot)$"
     "opacity 1.0 0.8, class:^(vesktop)$"
     "opacity 1.0 0.8, class:^(feishin)$"
