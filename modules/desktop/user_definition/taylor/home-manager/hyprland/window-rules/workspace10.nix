2a075a0e4656a0a1d40dd52917e27482a1389c8b
Taylor Sawhill <git@tsawhill.org>
2025-12-09 07:15:13 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/desktop/user_definition/taylor/home-manager/hyprland/window-rules/workspace10.nix
+++ modules/desktop/user_definition/taylor/home-manager/hyprland/window-rules/workspace10.nix
@@ -1,10 +1,10 @@
 {
   # Window rules for pseudotiled always-on apps for 2nd monitor
   wayland.windowManager.hyprland.settings.windowrule = [
-    "pseudo,match:class vesktop"
+    "pseudo on,match:class vesktop"
     "size 1920 1080,match:class vesktop"
 
-    "pseudo,match:class feishin"
+    "pseudo on,match:class feishin"
     "size 1000 1000,match:class feishin"
   ];
 }
