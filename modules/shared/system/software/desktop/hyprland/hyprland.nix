da31b71b0cc133dcf43f3fb587c68cd5653dc9b8
Taylor Sawhill <git@tsawhill.org>
2025-12-16 14:46:25 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/system/software/desktop/hyprland/hyprland.nix
+++ modules/shared/system/software/desktop/hyprland/hyprland.nix
@@ -11,9 +11,9 @@
   # Hyprland Config
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # portalPackage =
+    #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     withUWSM = true;
     xwayland.enable = true;
   };
