9a0cca594ae326cc306c76fb3525c2199a889a8c
Taylor Sawhill <git@tsawhill.org>
2025-12-16 18:12:12 -0800

    taylor-nix - Generation  Successful Build (No flake update)

--- modules/shared/user_definition/taylor/home-manager/hypr/hyprland/appearance.nix
+++ modules/shared/user_definition/taylor/home-manager/hypr/hyprland/appearance.nix
@@ -1,39 +1,39 @@
 {
   # Decoration settings
   wayland.windowManager.hyprland.settings.decoration = {
-    rounding = 5;
-    # blur = {
-    #   enabled = false;
-    #   size = 10;
-    # };
-    # shadow = {
-    #   enabled = false;
-    #   range = 4;
-    #   render_power = 3;
-    #   color = "rgba(1a1a1aee)";
-    # };
+    rounding = 2;
+    blur = {
+      enabled = "true";
+      size = 10;
+    };
+    shadow = {
+      enabled = "true";
+      range = 6;
+      render_power = 2;
+      color = "rgba(1a1a1aee)";
+    };
   };
 
   # Animation settings
-  # wayland.windowManager.hyprland.settings.animations = {
-  #   enabled = "no";
-  #   bezier = [
-  #     "linear, 0.5, 0.5, 0.5, 0.5"
-  #     "myBezier, 0.05, 0.9, 0.1, 1.05"
-  #   ];
-  #   animation = [
-  #     "windows, 1, 7, myBezier, popin 80%"
-  #     "windowsOut, 1, 7, default, popin 80%"
-  #     "border, 1, 10, default"
-  #     "borderangle, 1, 50, linear, loop"
-  #     "fade, 1, 7, default"
-  #     "workspaces, 1, 6, default, slidefade 20%"
-  #   ];
-  # };
+  wayland.windowManager.hyprland.settings.animations = {
+    enabled = "true";
+    bezier = [
+      "linear, 0.5, 0.5, 0.5, 0.5"
+      "myBezier, 0.05, 0.9, 0.1, 1.05"
+    ];
+    animation = [
+      "windows, 1, 7, myBezier, popin 80%"
+      "windowsOut, 1, 7, default, popin 80%"
+      # "border, 1, 10, default"
+      # "borderangle, 1, 50, linear, loop"
+      "fade, 1, 7, default"
+      "workspaces, 1, 6, default, slidefade 20%"
+    ];
+  };
   wayland.windowManager.hyprland.settings.general = {
-    gaps_in = 8;
-    gaps_out = 20;
-    border_size = 3;
+    gaps_in = 2;
+    gaps_out = 26;
+    border_size = 4;
     # "col.active_border" = "rgba(ffcce6d9) rgba(9778D0FF) 45deg";
     # "col.inactive_border" = "rgba(595959aa)";
   };
