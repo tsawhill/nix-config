6c1e1d67740a609c33b1a67cdefe6d4c2381c827
Taylor Sawhill <git@tsawhill.org>
2026-01-30 16:36:58 -0800

    Working authentik

--- modules/software/services/authentik.nix
+++ modules/software/services/authentik.nix
@@ -13,8 +13,16 @@
         use_ssl = false;
         from = "authentik@example.com";
       };
+      listen = {
+        http = "0.0.0.0:9000";
+        https = "0.0.0.0:9443";
+        # metrics = "0.0.0.0:9300"; # Optional: if you need metrics exposed
+      };
       disable_startup_analytics = true;
       avatars = "initials";
     };
   };
+  networking.firewall.allowedTCPPorts = [
+    9000
+  ];
 }
