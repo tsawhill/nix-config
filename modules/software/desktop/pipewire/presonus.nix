191477a012eda8660e719bdc769e4e2d7f374f88
Taylor Sawhill <git@tsawhill.org>
2026-04-02 13:51:47 -0700

    auto: manual deploy taylor-desktop-nix 2026-04-02 13:51

--- modules/software/desktop/pipewire/presonus.nix
+++ modules/software/desktop/pipewire/presonus.nix
@@ -31,28 +31,12 @@ in
               "node.class" = "Stream";
               "filter.graph" = {
                 nodes = [
-                  # Gate (threshold -27dB, reduction -40dB, attack 5ms, release 100ms)
                   {
-                    factory = "lv2";
-                    plugin = "http://calf.sourceforge.net/plugins/Gate";
-                  }
-                  # Deesser (de-esser for sibilance control)
-                  {
-                    factory = "lv2";
-                    plugin = "http://calf.sourceforge.net/plugins/Deesser";
-                  }
-                  # Compressor (dynamic range compression)
-                  {
-                    factory = "lv2";
-                    plugin = "http://calf.sourceforge.net/plugins/Compressor";
+                    type = "builtin";
+                    name = "passthrough";
                   }
                 ];
-                links = [
-                  { output = "Gate:audio_out_1"; input = "Deesser:audio_in_1"; }
-                  { output = "Gate:audio_out_2"; input = "Deesser:audio_in_2"; }
-                  { output = "Deesser:audio_out_1"; input = "Compressor:audio_in_1"; }
-                  { output = "Deesser:audio_out_2"; input = "Compressor:audio_in_2"; }
-                ];
+                links = [];
               };
             };
           }
