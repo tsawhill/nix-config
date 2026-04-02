bc9783a5f9c091be1788093b30c76126fd9f6df2
Taylor Sawhill <git@tsawhill.org>
2026-04-02 08:15:11 -0700

    auto: Daily pre-deploy 2026-04-02 08:15

--- hosts/taylor-desktop-nix/system/samba.nix
+++ hosts/taylor-desktop-nix/system/samba.nix
@@ -18,7 +18,7 @@
     fsType = "cifs";
     options =
       let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        automount_opts = "x-systemd.automount,noauto,nofail,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in
       [ "${automount_opts},credentials=/run/secrets/smb-nix-config-credentials,uid=1000,mfsymlinks" ];
   };
