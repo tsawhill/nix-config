81186928a9e79cfeec4ccd53b5af993af8c8375f
Taylor Sawhill <git@tsawhill.org>
2026-04-01 20:57:26 -0700

    auto: manual deploy taylor-desktop-nix 2026-04-01 20:57

--- modules/software/services/openrgb.nix
+++ modules/software/services/openrgb.nix
@@ -1,15 +1,38 @@
-{ pkgs, ... }:
 {
-  services.hardware.openrgb = {
-    enable = true;
-    package = pkgs.openrgb-with-all-plugins;
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.my.openrgb;
+  smBusModule = if cfg.cpuVendor == "intel" then "i2c-i801" else "i2c-piix4";
+in
+{
+  options.my.openrgb.cpuVendor = lib.mkOption {
+    type = lib.types.enum [
+      "amd"
+      "intel"
+    ];
+    default = "amd";
+    description = "CPU vendor, used to select the correct SMBus kernel module for OpenRGB.";
   };
 
-  services.udev.packages = [ pkgs.openrgb-with-all-plugins ];
+  config = {
+    services.hardware.openrgb = {
+      enable = true;
+      package = pkgs.openrgb-with-all-plugins;
+    };
 
-  # i2c is required for most RGB controllers
-  hardware.i2c.enable = true;
-  boot.kernelModules = [ "i2c-dev" ];
+    services.udev.packages = [ pkgs.openrgb-with-all-plugins ];
 
-  environment.systemPackages = [ pkgs.openrgb-with-all-plugins ];
+    # i2c is required for most RGB controllers
+    hardware.i2c.enable = true;
+    boot.kernelModules = [
+      "i2c-dev"
+      smBusModule
+    ];
+
+    environment.systemPackages = [ pkgs.openrgb-with-all-plugins ];
+  };
 }
