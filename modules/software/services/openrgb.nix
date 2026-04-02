841fd93582fc8d5651f29eed0b24997d0cbca415
Taylor Sawhill <git@tsawhill.org>
2026-04-02 06:50:52 -0700

    auto: manual deploy taylor-desktop-nix 2026-04-02 06:50

--- modules/software/services/openrgb.nix
+++ modules/software/services/openrgb.nix
@@ -9,13 +9,21 @@ let
   smBusModule = if cfg.cpuVendor == "intel" then "i2c-i801" else "i2c-piix4";
 in
 {
-  options.my.openrgb.cpuVendor = lib.mkOption {
-    type = lib.types.enum [
-      "amd"
-      "intel"
-    ];
-    default = "amd";
-    description = "CPU vendor, used to select the correct SMBus kernel module for OpenRGB.";
+  options.my.openrgb = {
+    cpuVendor = lib.mkOption {
+      type = lib.types.enum [
+        "amd"
+        "intel"
+      ];
+      default = "amd";
+      description = "CPU vendor, used to select the correct SMBus kernel module for OpenRGB.";
+    };
+
+    users = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "taylor" ];
+      description = "Users to add to the plugdev group for USB RGB device access.";
+    };
   };
 
   config = {
@@ -37,5 +45,7 @@ in
     boot.kernelParams = lib.mkIf (cfg.cpuVendor == "amd") [ "acpi_enforce_resources=lax" ];
 
     environment.systemPackages = [ pkgs.openrgb-with-all-plugins ];
+
+    users.users = lib.genAttrs cfg.users (_: { extraGroups = [ "plugdev" ]; });
   };
 }
