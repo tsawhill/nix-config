d34e74d748e2716fa7e6d6f39fa39daebe2d70e2
Taylor Sawhill <git@tsawhill.org>
2026-04-03 11:15:04 -0700

    auto: manual deploy taylor-desktop-nix 2026-04-03 11:15

--- modules/home-manager/gui/hypr/hyprland/window-rules/runelite.nix
+++ modules/home-manager/gui/hypr/hyprland/window-rules/runelite.nix
@@ -3,5 +3,8 @@
     "float on, match:class net-runelite-client-RuneLite"
     # Size rule only for the main client, not popups
     "size 1078 777, match:class net-runelite-client-RuneLite, match:title ^RuneLite$"
+
+    "float on, match:class BoltLauncher"
+    "size 1115 894, match:class BoltLauncher"
   ];
 }
