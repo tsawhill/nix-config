96a2a09359d2437e866462a092f30083c1da6939
Taylor Sawhill <git@tsawhill.org>
2026-04-15 09:18:29 -0700

    auto: manual deploy taylor-desktop-nix 2026-04-15 09:18

--- modules/home-manager/gui/hypr/hyprland/monitors/alienware165.nix
+++ modules/home-manager/gui/hypr/hyprland/monitors/alienware165.nix
@@ -13,7 +13,7 @@
     wayland.windowManager.hyprland.extraConfig = ''
       monitorv2 {
         output=desc:Dell Inc. AW3423DWF 3D442S3
-        mode=3440x1440@60Hz
+        mode=3440x1440@165Hz
         position=auto-right
         scale=1
         bitdepth=10
