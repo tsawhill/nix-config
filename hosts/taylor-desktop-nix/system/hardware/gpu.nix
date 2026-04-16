b61777a5d2c1b63c973d0d9c2277a1cdc9263b44
Taylor Sawhill <git@tsawhill.org>
2026-04-16 07:53:32 -0700

    auto: manual deploy taylor-desktop-nix 2026-04-16 07:53

--- hosts/taylor-desktop-nix/system/hardware/gpu.nix
+++ hosts/taylor-desktop-nix/system/hardware/gpu.nix
@@ -3,7 +3,7 @@ let
   # Udev rules to create stable symlinks for igpu/dgpu by PCI address
   igpu = pkgs.writeTextFile {
     name = "igpu-udev";
-    text = ''KERNEL=="card*", KERNELS=="0000:6e:00.0", SUBSYSTEM=="drm", SUBSYSTEMS=="pci", SYMLINK+="dri/amd-igpu"'';
+    text = ''KERNEL=="card*", KERNELS=="0000:6f:00.0", SUBSYSTEM=="drm", SUBSYSTEMS=="pci", SYMLINK+="dri/amd-igpu"'';
     destination = "/etc/udev/rules.d/amd-igpu-dev-path.rules";
   };
   dgpu = pkgs.writeTextFile {
