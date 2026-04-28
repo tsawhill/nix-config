51b1dc17b235115adc6368d2172f23c25277eb92
Taylor Sawhill <git@tsawhill.org>
2026-04-28 14:13:04 -0700

    auto: manual deploy taylor-desktop-nix 2026-04-28 14:13

--- modules/home-manager/gui/hypr/hyprland/monitors/alienware165.nix
+++ modules/home-manager/gui/hypr/hyprland/monitors/alienware165.nix
@@ -10,32 +10,23 @@
 
   config = lib.mkIf config.my.hypr.monitors.alienware165.enable {
 
-    home.activation.writeAlienware165MonitorConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      mkdir -p "$HOME/.config/hypr/monitors"
-      cat > "$HOME/.config/hypr/monitors/alienware165.conf" << 'MONITOREOF'
-monitorv2 {
-  output=desc:Dell Inc. AW3423DWF 3D442S3
-  mode=3440x1440@165Hz
-  position=auto-right
-  scale=1
-  bitdepth=10
-  supports_hdr=true
-  cm=hdr
-  sdr_min_luminance = 0.005
-  sdr_max_luminance = 240
-  min_luminance = 0
-  max_luminance = 400
-  max_avg_luminance = 300
-  supports_wide_color = true
-  sdr_brightness = 1.00
-  vrr=0
-}
-MONITOREOF
-    '';
-
     wayland.windowManager.hyprland.extraConfig = ''
-      exec-once = sed -i 's/sdr_brightness = [0-9.]*/sdr_brightness = 1.00/' ${config.home.homeDirectory}/.config/hypr/monitors/alienware165.conf
-      source = ${config.home.homeDirectory}/.config/hypr/monitors/alienware165.conf
+      monitorv2 {
+        output=desc:Dell Inc. AW3423DWF 3D442S3
+        mode=3440x1440@165Hz
+        position=auto-right
+        scale=1
+        bitdepth=10
+        supports_hdr=true
+        cm=hdr
+        sdr_min_luminance = 0.005
+        sdr_max_luminance = 240
+        min_luminance = 0
+        max_luminance = 400
+        max_avg_luminance = 300
+        supports_wide_color = true
+        vrr=0
+      }
     '';
   };
 }
--- modules/home-manager/gui/hypr/hyprland/monitors/alienware360.nix
+++ modules/home-manager/gui/hypr/hyprland/monitors/alienware360.nix
@@ -10,32 +10,23 @@
 
   config = lib.mkIf config.my.hypr.monitors.alienware360.enable {
 
-    home.activation.writeAlienware360MonitorConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      mkdir -p "$HOME/.config/hypr/monitors"
-      cat > "$HOME/.config/hypr/monitors/alienware360.conf" << 'MONITOREOF'
-monitorv2 {
-  output=desc:Dell Inc. AW2725DF CJ56ZZ3
-  mode=2560x1440@360Hz
-  position=0x0
-  scale=1
-  bitdepth=10
-  supports_hdr=true
-  cm=hdr
-  sdr_min_luminance = 0.005
-  sdr_max_luminance = 240
-  min_luminance = 0
-  max_luminance = 400
-  max_avg_luminance = 300
-  supports_wide_color = true
-  sdr_brightness = 1.00
-  vrr=0
-}
-MONITOREOF
-    '';
-
     wayland.windowManager.hyprland.extraConfig = ''
-      exec-once = sed -i 's/sdr_brightness = [0-9.]*/sdr_brightness = 1.00/' ${config.home.homeDirectory}/.config/hypr/monitors/alienware360.conf
-      source = ${config.home.homeDirectory}/.config/hypr/monitors/alienware360.conf
+      monitorv2 {
+        output=desc:Dell Inc. AW2725DF CJ56ZZ3
+        mode=2560x1440@360Hz
+        position=0x0
+        scale=1
+        bitdepth=10
+        supports_hdr=true
+        cm=hdr
+        sdr_min_luminance = 0.005
+        sdr_max_luminance = 240
+        min_luminance = 0
+        max_luminance = 400
+        max_avg_luminance = 300
+        supports_wide_color = true
+        vrr=0
+      }
     '';
   };
 }
