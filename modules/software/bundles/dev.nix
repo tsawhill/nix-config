e93357ed41c4b1a182ad2caf81e6cf494d54c33a
Taylor Sawhill <git@tsawhill.org>
2026-04-29 09:57:51 -0700

    auto: manual deploy taylor-desktop-nix 2026-04-29 09:57

--- modules/software/bundles/dev.nix
+++ modules/software/bundles/dev.nix
@@ -15,6 +15,14 @@ let
       outputHash = "sha256-f+6xXZVb5sYrmrH7eoon6/QoQaTnBuTnb+YnvszqyKA=";
     };
   });
+  chatgptExt = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "chatgpt";
+      publisher = "openai";
+      version = "26.5422.71525"; # put actual version
+      sha256 = "sha256-16qkyn5xhh6clchx2c7w4y9ly82kj576dy3ha9q3ljj8sk3sp7gm";
+    };
+  };
 in
 {
   options.software.dev.enable = lib.mkEnableOption "development tools";
@@ -24,14 +32,15 @@ in
     programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
-      extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
-        esbenp.prettier-vscode
-        ms-python.vscode-pylance
-        github.copilot-chat
-        claude-code-ext
-        genieai.chatgpt-vscode
-      ];
+      extensions =
+        (with pkgs.vscode-extensions; [
+          jnoortheen.nix-ide
+          esbenp.prettier-vscode
+          ms-python.vscode-pylance
+          github.copilot-chat
+          claude-code-ext
+        ])
+        ++ [ openai-chatgpt ];
     };
 
     environment.systemPackages = with pkgs; [
