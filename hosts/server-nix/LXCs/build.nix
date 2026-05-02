43c93a8725fb33ae1771b4b92752005a8c42e702
Taylor Sawhill <git@tsawhill.org>
2026-05-01 20:54:55 -0700

    auto: manual deploy build-nix 2026-05-01 20:54

--- hosts/server-nix/LXCs/build.nix
+++ hosts/server-nix/LXCs/build.nix
@@ -26,8 +26,14 @@
   };
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   nix.settings = {
-    substituters = [ "https://nix-community.cachix.org" ];
-    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
   };
   networking.hostName = "build-nix";
 }
