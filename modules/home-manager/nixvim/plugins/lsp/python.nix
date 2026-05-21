67cead8ce6fbac144e20939b826e0f3a004f0551
Taylor Sawhill <git@tsawhill.org>
2026-05-20 23:59:18 -0700

    auto: manual deploy taylor-desktop-nix 2026-05-20 23:59

--- modules/home-manager/nixvim/plugins/lsp/python.nix
+++ modules/home-manager/nixvim/plugins/lsp/python.nix
@@ -1,9 +1,17 @@
+{ pkgs, ... }:
+let
+  pylspPackage = pkgs.python3Packages.python-lsp-server.overridePythonAttrs (_old: {
+    # 1.14.0 currently times out/hangs in pytest on Python 3.13, blocking deploys.
+    doCheck = false;
+  });
+in
 {
   programs.nixvim.plugins = {
     lsp = {
       servers = {
         pylsp = {
           enable = true;
+          package = pylspPackage;
           settings.plugins = {
             black.enabled = true;
             flake8.enabled = false;
