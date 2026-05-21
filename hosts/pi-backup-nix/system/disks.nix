b0a3ab7f66a2c4508dfc6a0ad36052a3909708e0
Taylor Sawhill <git@tsawhill.org>
2026-05-20 23:49:45 -0700

    auto: manual deploy pi-backup-nix 2026-05-20 23:49

--- hosts/pi-backup-nix/system/disks.nix
+++ hosts/pi-backup-nix/system/disks.nix
@@ -1,23 +1,11 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
-let
-  hdparm = lib.getExe pkgs.hdparm;
-in
 {
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.extraPools = [ "backup" ];
   boot.zfs.devNodes = "/dev/disk/by-id";
 
   networking.hostId = "8425e349";
-
-  # The backup pool is normally idle outside the nightly receive/prune window.
-  # Set the three pool disks to enter standby after 30 minutes of device idle
-  # time. hdparm encodes 30 minutes as `-S 241`.
-  services.udev.extraRules = ''
-    ACTION=="add|change", SUBSYSTEM=="block", ENV{DEVTYPE}=="disk", ENV{ID_SERIAL}=="Hitachi_HUA723030ALA640_MK0371YHK6P13A", RUN+="${hdparm} -S 241 /dev/%k"
-    ACTION=="add|change", SUBSYSTEM=="block", ENV{DEVTYPE}=="disk", ENV{ID_SERIAL}=="WDC_WD30EZRZ-00WN9B0_WD-WCC4E7KF51NR", RUN+="${hdparm} -S 241 /dev/%k"
-    ACTION=="add|change", SUBSYSTEM=="block", ENV{DEVTYPE}=="disk", ENV{ID_SERIAL}=="Hitachi_HUA723030ALA640_MK0371YHJZLJ0A", RUN+="${hdparm} -S 241 /dev/%k"
-  '';
 
   # Keep the backup pool mounted at the host-local mountpoint without forcing a
   # remount during every activation. `zfs set -u` updates the property without
