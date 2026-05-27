c61a00e36e270ab7886254b6d4c9fec8bab1175a
Taylor Sawhill <git@tsawhill.org>
2026-05-27 14:43:59 -0700

    auto: manual deploy build-nix 2026-05-27 14:43

--- hosts/server-nix/LXCs/build.nix
+++ hosts/server-nix/LXCs/build.nix
@@ -57,6 +57,13 @@
       "kopuz.cachix.org-1:J2X3AnAYhKTJW5S3aCLoA1ckonQXVNZMQvhZA0YAufw="
     ];
   };
+  environment.systemPackages =
+    let
+      pkgs-master = import inputs.nixpkgs-master {
+        system = "x86_64-linux";
+      };
+    in
+    [ pkgs-master.claude-code ];
   my.garbage.collection.prunePerHostProfiles = true;
   networking.hostName = "build-nix";
 }
