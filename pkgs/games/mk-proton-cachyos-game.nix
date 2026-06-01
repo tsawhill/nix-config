8b40f591dcbe553135d2510085daf923b5d1e130
Taylor Sawhill <git@tsawhill.org>
2026-06-01 14:16:10 -0700

    auto: manual deploy taylor-desktop-nix 2026-06-01 14:16

--- pkgs/games/mk-proton-cachyos-game.nix
+++ pkgs/games/mk-proton-cachyos-game.nix
@@ -18,6 +18,14 @@
   lsfgVkEnable ? false,
 }:
 let
+  shellPath =
+    path:
+    if path == "~" then
+      "$HOME"
+    else if lib.hasPrefix "~/" path then
+      "$HOME/${lib.escapeShellArg (lib.removePrefix "~/" path)}"
+    else
+      lib.escapeShellArg path;
   effectiveEnv = env ++ lib.optionals lsfgVkEnable [ "DISABLE_LSFG=0" ];
   envExports = lib.concatStringsSep "\n" (
     map (assignment: "export ${lib.escapeShellArg assignment}") effectiveEnv
@@ -38,18 +46,8 @@ let
     text = ''
       set -euo pipefail
 
-      exe_path=${lib.escapeShellArg exePath}
-      prefix_path=${lib.escapeShellArg prefixPath}
-
-      case "$exe_path" in
-        "~") exe_path="$HOME" ;;
-        "~/"*) exe_path="$HOME/''${exe_path#~/}" ;;
-      esac
-
-      case "$prefix_path" in
-        "~") prefix_path="$HOME" ;;
-        "~/"*) prefix_path="$HOME/''${prefix_path#~/}" ;;
-      esac
+      exe_path=${shellPath exePath}
+      prefix_path=${shellPath prefixPath}
 
       game_dir="''${exe_path%/*}"
       if [ "$game_dir" = "$exe_path" ]; then
