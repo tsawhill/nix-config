3c2fbf45dd7042b22d4cf6c820678c92d8c13b0a
Taylor Sawhill <git@tsawhill.org>
2026-06-03 17:22:05 -0700

    Update syncthing IDs

--- hosts/taylor-desktop-nix/system/syncthing.nix
+++ hosts/taylor-desktop-nix/system/syncthing.nix
@@ -9,18 +9,28 @@
     cert = "/etc/syncthing/cert.pem";
     settings = {
       options.relaysEnabled = false;
-      devices."server" = {
-        id = "JTHKLGM-CN3PZGC-ZZGSJMR-JERGJL3-NJ4JPW6-CRGEDFJ-MXA7D36-UT2SFQR";
-        addresses = [ "tcp://syncthing-nix.lan:22000" ];
+      optiosn.globalAnnounceEnabled = false;
+      devices = {
+        "thor".id = "UZKUGQ5-YZUACUX-UM7UKVH-ODTT5B3-4SSZUJ6-YI7H4XH-WZXSJMM-3AWQOA6";
+        "server" = {
+          id = "SCTSBHY-JM3BRIK-XOPRR5X-NJCFOT5-E2MUNII-WWEJYTV-FR2UUWG-76OAFAL";
+          addresses = [ "tcp://syncthing-nix.lan:22000" ];
+        };
       };
       folders = {
         "roms" = {
           path = "/home/taylor/Games/roms";
-          devices = [ "server" ];
+          devices = [
+            "server"
+            "thor"
+          ];
         };
         "gamesaves" = {
           path = "/home/taylor/Games/saves";
-          devices = [ "server" ];
+          devices = [
+            "server"
+            "thor"
+          ];
         };
       };
     };
