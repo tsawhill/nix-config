59b7c5972a5c93bc34b1b2c4e0076a69e813b32b
Taylor Sawhill <git@tsawhill.org>
2026-06-06 08:12:52 -0700

    auto: manual deploy adguard-nix 2026-06-06 08:12

--- modules/home-manager/nixvim/default.nix
+++ modules/home-manager/nixvim/default.nix
@@ -12,33 +12,35 @@
 
   options.my.nixvim.full = lib.mkEnableOption "the full Nixvim plugin set with LSPs and heavier language tooling";
 
-  programs.nixvim = {
-    enable = true;
-    nixpkgs.useGlobalPackages = true;
-    clipboard.register = "unnamedplus";
-    globals.mapleader = " "; # Sets leader to Space
-    colorschemes.catppuccin = {
+  config = {
+    programs.nixvim = {
       enable = true;
-      settings.flavour = "frappe";
+      nixpkgs.useGlobalPackages = true;
+      clipboard.register = "unnamedplus";
+      globals.mapleader = " "; # Sets leader to Space
+      colorschemes.catppuccin = {
+        enable = true;
+        settings.flavour = "frappe";
+      };
+      opts = {
+        number = true;
+      };
+      # Inject the OSC 52 clipboard logic for SSH sessions natively
+      extraConfigLua = ''
+        if vim.env.SSH_TTY then
+          vim.g.clipboard = {
+            name = 'OSC 52',
+            copy = {
+              ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+              ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+            },
+            paste = {
+              ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+              ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+            },
+          }
+        end
+      '';
     };
-    opts = {
-      number = true;
-    };
-    # Inject the OSC 52 clipboard logic for SSH sessions natively
-    extraConfigLua = ''
-      if vim.env.SSH_TTY then
-        vim.g.clipboard = {
-          name = 'OSC 52',
-          copy = {
-            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-          },
-          paste = {
-            ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-            ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-          },
-        }
-      end
-    '';
   };
 }
