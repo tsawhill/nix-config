c03e8a0f09c7b89a90bb5103288d5a51baf80485
Taylor Sawhill <git@tsawhill.org>
2026-06-10 09:49:41 -0700

    auto: manual deploy taylor-deck-nix 2026-06-10 09:49

--- modules/hardware/udev/hidraw.nix
+++ modules/hardware/udev/hidraw.nix
@@ -1,12 +1,12 @@
 { lib, config, ... }:
 {
-  options.my.udev.hidraw.enable = lib.mkEnableOption "hidraw access for plugdev group" // {
+  options.my.udev.hidraw.enable = lib.mkEnableOption "hidraw access for input group" // {
     default = true;
   };
 
   config = lib.mkIf config.my.udev.hidraw.enable {
     services.udev.extraRules = ''
-      SUBSYSTEM=="hidraw", GROUP="plugdev", MODE="0660"
+      SUBSYSTEM=="hidraw", GROUP="input", MODE="0660", TAG+="uaccess"
     '';
   };
 }
