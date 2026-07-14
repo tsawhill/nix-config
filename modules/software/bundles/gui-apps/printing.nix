9bbadfdba6edb767e77a892b947638cf01839dfb
Taylor Sawhill <git@tsawhill.org>
2026-07-13 19:27:24 -0700

    auto: Daily pre-deploy 2026-07-13 19:27

--- modules/software/bundles/gui-apps/printing.nix
+++ modules/software/bundles/gui-apps/printing.nix
@@ -1,11 +1,15 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   options.software.apps.printing.enable = lib.mkEnableOption "3D printing and modeling tools";
 
   config = lib.mkIf config.software.apps.printing.enable {
     environment.systemPackages = with pkgs; [
       orca-slicer
-      freecad
       blender
     ];
   };
