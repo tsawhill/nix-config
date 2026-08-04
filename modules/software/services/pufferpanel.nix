01d60648919182db732a5533a423decb71e29a56
Taylor Sawhill <git@tsawhill.org>
2026-08-04 14:24:55 -0700

    fix(pufferpanel): run downloaded tools in FHS environment

--- modules/software/services/pufferpanel.nix
+++ modules/software/services/pufferpanel.nix
@@ -1,8 +1,25 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   services.pufferpanel = {
     enable = true;
-    extraPackages = [ pkgs.javaPackages.compiler.openjdk21 ];
+    extraPackages = with pkgs; [
+      bash
+      curl
+      gawk
+      gnutar
+      gzip
+      javaPackages.compiler.openjdk21
+    ];
+    package = pkgs.buildFHSEnv {
+      name = "pufferpanel-fhs";
+      runScript = lib.getExe pkgs.pufferpanel;
+      targetPkgs =
+        pkgs': with pkgs'; [
+          icu
+          openssl
+          zlib
+        ];
+    };
   };
   networking.firewall.allowedTCPPorts = [ 8080 ];
 }
