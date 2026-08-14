c6a2de70e7e0818e15fa13dd0aa7f202ab73cfff
Taylor Sawhill <git@tsawhill.org>
2026-08-14 13:06:17 -0700

    games: add Skyrim Special Edition launchers

---
+++ modules/software/games/proton/skyrim-anniversary-edition.nix
@@ -0,0 +1,21 @@
+{
+  software.games.entries = {
+    skyrimAnniversaryEdition = {
+      command = "skyrim";
+      desktopName = "The Elder Scrolls V: Skyrim Special Edition";
+      category = "The Elder Scrolls";
+      basePath = "pc/TES - Skyrim - Anniversary Edition";
+      runner.umu.exe = "skse64_loader.exe";
+    };
+
+    skyrimAnniversaryEditionLauncher = {
+      command = "skyrim-launcher";
+      desktopName = "Skyrim Special Edition Launcher";
+      category = "The Elder Scrolls";
+      gamescope.resolutions = [ ];
+      lsfgVk.enable = false;
+      basePath = "pc/TES - Skyrim - Anniversary Edition";
+      runner.umu.exe = "SkyrimSELauncher.exe";
+    };
+  };
+}
