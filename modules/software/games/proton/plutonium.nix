b100afd7fc171d4be561375b0471cf8c929ede1b
Taylor Sawhill <git@tsawhill.org>
2026-08-30 17:15:10 -0700

    deploy(taylor-desktop-nix): Enable network support for Plutonium on tay…
    - plutonium.nix: Added network.enable = true;
    
    taylor-desktop-nix: 26.11pre-git (Zokor)
    
    Deploy-Base: a450a67fceaeaca18a629c9ee4307aca16130bce

--- modules/software/games/proton/plutonium.nix
+++ modules/software/games/proton/plutonium.nix
@@ -3,6 +3,7 @@
     command = "plutonium";
     desktopName = "Plutonium";
     basePath = "pc/Call of Duty/Plutonium";
+    network.enable = true;
     runner.umu = {
       exe = "plutonium.exe";
       proton = "ge-proton";
