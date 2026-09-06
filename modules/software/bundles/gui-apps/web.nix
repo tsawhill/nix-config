1fa01ca1c22ee2a852290dabef3cca0cfd214925
Taylor Sawhill <git@tsawhill.org>
2026-09-06 10:34:07 -0700

    deploy(taylor-desktop-nix): Update web browser to include H.264/AAC sup…
    - gui-apps/web.nix: Updated zen-browser to include H.264/AAC runtime libraries by overriding the default package.
    
    taylor-desktop-nix: 26.11pre-git (Zokor)
    
    Deploy-Base: 0b7a9c80abcae966e14fa8d8f66a8fc9a0bebe43

--- modules/software/bundles/gui-apps/web.nix
+++ modules/software/bundles/gui-apps/web.nix
@@ -6,12 +6,24 @@
   zen-input,
   ...
 }:
+let
+  zenPackages = zen-input.packages.${pkgs.stdenv.hostPlatform.system};
+  zenBrowser = zenPackages.default.override {
+    zen-browser-unwrapped = zenPackages.zen-browser-unwrapped.overrideAttrs (old: {
+      # The pinned Zen flake still uses ffmpegSupport; current wrapFirefox
+      # checks withFFmpeg before adding the H.264/AAC runtime libraries.
+      passthru = (old.passthru or { }) // {
+        withFFmpeg = true;
+      };
+    });
+  };
+in
 {
   options.software.apps.web.enable = lib.mkEnableOption "web browsers and related tools";
 
   config = lib.mkIf config.software.apps.web.enable {
     environment.systemPackages = with pkgs; [
-      zen-input.packages.${pkgs.stdenv.hostPlatform.system}.default
+      zenBrowser
       ungoogled-chromium
     ];
   };
