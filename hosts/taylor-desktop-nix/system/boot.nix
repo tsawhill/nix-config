daf1724b83639a777f1f56487ca35c00d905cfca
Taylor Sawhill <git@tsawhill.org>
2026-09-07 14:54:00 -0700

    deploy(taylor-desktop-nix): Update kernel and ZFS packages on taylor-de…
    - linux 6.18.49 -> 7.2.3
    - zfs-kernel 2.4.4-6.18.49 -> 2.4.4-7.2.3
    
    taylor-desktop-nix: 26.11pre-git (Zokor)
    
    Deploy-Base: 36a8d1981f97754472d1e2266baedc7c287637dd

--- hosts/taylor-desktop-nix/system/boot.nix
+++ hosts/taylor-desktop-nix/system/boot.nix
@@ -1,5 +1,29 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
+let
+  # Follow the newest versioned kernel supported by our configured ZFS package.
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    builtins.match "linux_[0-9]+_[0-9]+" name != null
+    && (
+      let
+        zfsCheck = builtins.tryEval (
+          kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken
+        );
+      in
+      zfsCheck.success && !zfsCheck.value
+    )
+  ) pkgs.linuxKernel.packages;
+  sortedKernelPackages = lib.sort (a: b: lib.versionOlder a.kernel.version b.kernel.version) (
+    builtins.attrValues zfsCompatibleKernelPackages
+  );
+in
 {
+  boot.kernelPackages =
+    if sortedKernelPackages == [ ] then
+      throw "No ZFS-compatible kernel found for taylor-desktop-nix"
+    else
+      lib.last sortedKernelPackages;
+
   boot.loader.efi.canTouchEfiVariables = true;
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
 
